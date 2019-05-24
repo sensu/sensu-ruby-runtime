90b47878b459c9d11900018aa655aafc7d4add49
Jef Spaleta <jef@sensu.io>
2019-05-23 22:48:36 -0800

    make separate centos6 and centos7 images

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-platforms=( alpine debian centos )
+platforms=( alpine debian centos centos7 centos6 )
 ruby_version=2.4.4
 asset_version=${TRAVIS_TAG:-0.0.1}
 
 mkdir -p dist
+mkdir -p assets
 
 for platform in "${platforms[@]}"
 do
+    echo "Building Docker Image: sensu-ruby-runtime:${ruby_version}-${platform}"
     docker build --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t sensu-ruby-runtime:${ruby_version}-${platform} -f Dockerfile.${platform} .
-
+    echo "Making Asset: /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_amd64.tar.gz"
     docker run -v "$PWD/dist:/dist" sensu-ruby-runtime:${ruby_version}-${platform} cp /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_amd64.tar.gz /dist/
 done
