70c90a8ad0441f664171ac1d3dd4d96f295c02bd
Jef Spaleta <jef@sensu.io>
2019-06-07 09:14:24 +0000

    update names of docker images to encode ruby version and platform. Use tag to match github release

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 platforms=( alpine debian centos alpine3.8 debian9 centos7 centos6 )
 ruby_version=2.4.4
-asset_version=${TRAVIS_TAG:-0.0.1}
+asset_version=${TRAVIS_TAG:-local-build}
 
 mkdir -p dist
 mkdir -p assets
@@ -10,7 +10,7 @@ mkdir -p assets
 for platform in "${platforms[@]}"
 do
     echo "Building Docker Image: sensu-ruby-runtime:${ruby_version}-${platform}"
-    docker build --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t sensu-ruby-runtime:${ruby_version}-${platform} -f Dockerfile.${platform} .
+    docker build --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t sensu-ruby-runtime-${ruby_version}-${platform}:${asset_version} -f Dockerfile.${platform} .
     echo "Making Asset: /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_amd64.tar.gz"
-    docker run -v "$PWD/dist:/dist" sensu-ruby-runtime:${ruby_version}-${platform} cp /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_amd64.tar.gz /dist/
+    docker run -v "$PWD/dist:/dist" sensu-ruby-runtime-${ruby_version}-${platform}:${asset_version} cp /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_amd64.tar.gz /dist/
 done
