821bd12fe82fabfe64049d769e93fdf91a62d3b5
Jef Spaleta <jspaleta@gmail.com>
2020-12-08 16:26:12 -0900

    fix centos builds

--- build_scripts/build_centos_platforms.sh
+++ build_scripts/build_centos_platforms.sh
@@ -10,7 +10,7 @@ retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
 fi
-exit 0
+
 # CentOS 7 platform
 platform="centos7" test_platforms="centos:8 centos:7 debian:8 debian:9 debian:10 ubuntu:14.04 ubuntu:16.04 ubuntu:18.04 ubuntu:20.04" ./build_and_test_platform.sh
 retval=$?
