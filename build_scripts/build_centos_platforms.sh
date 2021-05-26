38ee68ce62c476424599eb69df6727adced22f3e
Jef Spaleta <jspaleta@gmail.com>
2021-05-26 15:19:44 -0800

    add centos6 build back in

--- build_scripts/build_centos_platforms.sh
+++ build_scripts/build_centos_platforms.sh
@@ -18,6 +18,14 @@ if [[ retval -ne 0 ]]; then
   exit $retval
 fi
 
+# CentOS 6 platform
+# Note: EOL Nov 30, 2020
+platform="centos6" test_platforms="centos:6 centos:7 centos:8 debian:8 debian:9 debian:10 ubuntu:14.04 ubuntu:16.04 ubuntu:18.04" ./build_and_test_platform.sh
+retval=$?
+if [[ retval -ne 0 ]]; then
+  exit $retval
+fi
+
 ## CentOS platform
 #platform="centos" test_platforms="centos:8 centos:7 debian:8 debian:9 debian:10 ubuntu:14.04 ubuntu:16.04 ubuntu:18.04 ubuntu:20.04" ./build_and_test_platform.sh
 #retval=$?
@@ -27,11 +35,4 @@ fi
 
 
 
-## CentOS 6 platform
-# Note: EOL Nov 30, 2020
-#platform="centos6" test_platforms="centos:6 centos:7 debian:8 debian:9 debian:10 ubuntu:14.04 ubuntu:16.04 ubuntu:18.04" ./build_and_test_platform.sh
-#retval=$?
-#if [[ retval -ne 0 ]]; then
-#  exit $retval
-#fi
 
