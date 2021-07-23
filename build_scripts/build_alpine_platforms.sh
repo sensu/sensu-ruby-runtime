d8d292761de9bdc093d5dba14fa1a071c09440e4
Jef Spaleta <jspaleta@gmail.com>
2021-07-22 17:00:55 -0800

    update alpine to 3.14 release

--- build_scripts/build_alpine_platforms.sh
+++ build_scripts/build_alpine_platforms.sh
@@ -5,12 +5,7 @@ mkdir -p assets
 mkdir -p scripts
 
 # Alpine platform
-platform="alpine" test_platforms="alpine:latest alpine:3 alpine:3.8" ./build_and_test_platform.sh
-retval=$?
-if [[ retval -ne 0 ]]; then
-  exit $retval
-fi
-platform="alpine3.8" test_platforms="alpine:latest alpine:3 alpine:3.8" ./build_and_test_platform.sh
+platform="alpine" test_platforms="alpine:latest alpine:3" ./build_and_test_platform.sh
 retval=$?
 if [[ retval -ne 0 ]]; then
   exit $retval
