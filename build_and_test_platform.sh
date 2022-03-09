d28bd3579ea2f3c26a69bef7df8b223f193d3264
Michał Skibicki <michal@skibicki.net>
2022-03-09 19:15:26 +0100

    fix

--- build_and_test_platform.sh
+++ build_and_test_platform.sh
@@ -22,7 +22,7 @@ else
   if [[ "$(docker images -q ${asset_image} 2> /dev/null)" == "" ]]; then
     echo "Docker image not found...we can build"
     echo "Building Docker Image: sensu-ruby-runtime:${ruby_version}-${platform}"
-    DOCKER_CLI_EXPERIMENTAL=enabled docker buildx --platform "linux/arm64" --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t ${asset_image} -f Dockerfile.${platform} .
+    DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --platform "linux/arm64" --build-arg "RUBY_VERSION=$ruby_version" --build-arg "ASSET_VERSION=$asset_version" -t ${asset_image} -f Dockerfile.${platform} .
     echo "Making Asset: /assets/sensu-ruby-runtime_${asset_version}_ruby-${ruby_version}_${platform}_linux_$(uname -m).tar.gz"
     docker run -v "$PWD/dist:/dist" ${asset_image} cp /assets/${asset_filename} /dist/
   #    #rm $PWD/test/*
